3cdb0942d8babb18d3e47ca8f9e3b81958c05c44
boisselle <smb@gatech.edu>
2021-07-14 03:35:28 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,24 +16,44 @@ import Layout from "../components/layout"
 
 // export default IndexPage
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <>
+//       <Layout>
+//         <div className="singleColumn">
+//           <main>Now the index page works.</main>
+//           <h3>Myriam Boisselle</h3>
+//           <h2>Location Sound Mixer & Recordist</h2>
+//           <h2>San Francisco, Los Angeles, Miami, and more</h2>
+//           <p>(305) 502-5390</p>
+//           <p>metamorphsound@gmail.com</p>
+//           <p> insert photo element here...</p>
+//           <p>
+//             Proficient in mixing and recording double system sync sound with
+//             Arri, Red, Black Magic, Canon and Sony, & DSLRs.
+//           </p>
+//         </div>
+//       </Layout>
+//     </>
+//   )
+// }
+
+const IndexPage = () => {
   return (
     <>
       <Layout>
-        <div className="singleColumn">
-          <main>Now the index page works.</main>
-          <h3>Myriam Boisselle</h3>
-          <h2>Location Sound Mixer & Recordist</h2>
-          <h2>San Francisco, Los Angeles, Miami, and more</h2>
-          <p>(305) 502-5390</p>
-          <p>metamorphsound@gmail.com</p>
-          <p> insert photo element here...</p>
+        {/* <Seo title="Home" /> */}
+        <main className="mainPage">
+          <h1>MYRIAM BOISSELLE</h1>
+          <h2>LOCATION SOUND MIXER & RECORDIST</h2>
           <p>
-            Proficient in mixing and recording double system sync sound with
-            Arri, Red, Black Magic, Canon and Sony, & DSLRs.
+            Proficient in mixing and recording double system sync sound ​with
+            Arri, Red, Black Magic, Canon and Sony & DSLRs.
           </p>
-        </div>
+        </main>
       </Layout>
     </>
   )
 }
+
+export default IndexPage
